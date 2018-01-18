4bc4c06f4c2f5311f69e64643f729d38f56255a9
Daniel Leonett <daniel.leonett@globant.com>
2018-01-18 16:19:46 -0500

    Emoji input

--- app/src/main/java/com/example/danielleonett/myapplication/ui/main/MainActivity.java
+++ app/src/main/java/com/example/danielleonett/myapplication/ui/main/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.danielleonett.myapplication.ui.main;
 
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatTextView;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.example.danielleonett.myapplication.R;
 import com.example.danielleonett.myapplication.base.BaseActivity;
@@ -11,6 +14,9 @@ import com.jakewharton.rxbinding2.view.RxView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+import hani.momanii.supernova_emoji_library.Actions.EmojIconActions;
+import hani.momanii.supernova_emoji_library.Helper.EmojiconEditText;
 import io.reactivex.Observable;
 
 public class MainActivity extends BaseActivity implements MainMvpView {
@@ -22,6 +28,13 @@ public class MainActivity extends BaseActivity implements MainMvpView {
     AppCompatTextView labelUserName;
     @BindView(R.id.labelUserCountry)
     AppCompatTextView labelUserCountry;
+    @BindView(R.id.inputMessage)
+    EmojiconEditText inputMessage;
+    @BindView(R.id.btnShowEmojis)
+    ImageView btnShowEmojis;
+    @BindView(R.id.rootView)
+    LinearLayout rootView;
+    EmojIconActions emojIconActions;
 
     // Fields
     private MainMvpPresenter<MainMvpView> presenter;
@@ -44,6 +57,26 @@ public class MainActivity extends BaseActivity implements MainMvpView {
     @Override
     protected void initViews() {
         ButterKnife.bind(this);
+
+        emojIconActions = new EmojIconActions(this, rootView, inputMessage, btnShowEmojis);
+        emojIconActions.ShowEmojIcon();
+        emojIconActions.setKeyboardListener(new EmojIconActions.KeyboardListener() {
+            @Override
+            public void onKeyboardOpen() {
+                Log.e("Keyboard", "open");
+            }
+
+            @Override
+            public void onKeyboardClose() {
+                Log.e("Keyboard", "close");
+            }
+        });
+
+    }
+
+    @OnClick(R.id.btnSend)
+    public void onSendClick() {
+        Log.e("Keyboard", inputMessage.getText().toString());
     }
 
     @Override
